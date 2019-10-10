04da4399ec3f56ccac1bea1604f33438ec30fd80
Kseniya Dunchyk <Kseniya_Dunchyk@epam.com>
2019-10-10 13:18:28 +0300

    commiting NUnit Test for CircleCI

---
+++ primeCircle.Tests/CircleTest.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PrimeCircle;
+using System;
+using System.Threading;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace PrimeCircle.Tests
+{
+ [TestFixture]
+    public class Tests
+    {
+        
+        private const string VerifyMessage = "The assertion has failed";
+        IWebDriver driver;
+
+        [SetUp]
+        public void GetEstimation()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://cloud.google.com");
+          //  Thread.Sleep(3000);
+            Console.WriteLine("the page is opened");
+            driver.FindElement(By.XPath("//a[contains(text(),'See all 100+ products')]")).Click();
+        }
+
+        [Test]
+        public void VerifyVMClass()
+        {
+            Assert.AreEqual(2, 2, VerifyMessage);
+        }
+
+        [TearDown]
+        public void CloseDriver()
+        {
+            driver.Quit();
+            Console.WriteLine("the page is closed");
+        }
+    }
+}
---
+++ primeCircle/PrimeCircle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeCircle
+{
+    public class PrimeCircle
+    {
+	public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Please create a test first.");
+        }
+    }
+}
